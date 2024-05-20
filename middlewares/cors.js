3e8ef823709d16e0319a44bd5e1af70dd05f1651
Yachilenok <gohasav@icloud.com>
2024-05-20 20:21:17 +0300

    добавил ссылку фронта для доступа к серверу

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,8 @@
-const allowedCors = ["http://localhost:3001", "http://localhost:3000"];
+const allowedCors = [
+  "http://localhost:3001",
+  "http://localhost:3000",
+  "https://pindiegames.nomoredomainswork.ru/",
+];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
