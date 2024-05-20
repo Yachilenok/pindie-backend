b1eecf757c6fdcc066c987752c3bd5e235c6169c
Yachilenok <gohasav@icloud.com>
2024-05-20 21:06:39 +0300

    изменил ссылку фронта

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   "http://localhost:3001",
   "http://localhost:3000",
-  "https://pindiegames.nomoredomainswork.ru/",
+  "https://ychlnkpindiefrontend.nomoredomainswork.ru",
 ];
 
 function cors(req, res, next) {
