d0724cb5d113eb80d109a13111d4b9c12fad796b
Yachilenok <gohasav@icloud.com>
2024-05-24 18:57:05 +0300

    поправил работу роутеров для категории и игр

--- controllers/categories.js
+++ controllers/categories.js
@@ -18,9 +18,15 @@ const sendDeleteCategory = (req, res) => {
   res.end(JSON.stringify(req.category));
 };
 
+const sendCategoryById = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.category));
+};
+
 module.exports = {
   sendAllCategories,
   sendCreatedCategory,
   sendUpdatedCategory,
   sendDeleteCategory,
+  sendCategoryById,
 };
--- controllers/games.js
+++ controllers/games.js
@@ -18,9 +18,15 @@ const sendDeleteGame = (req, res) => {
   res.end(JSON.stringify(req.game));
 };
 
+const sendGameById = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.game));
+}; 
+
 module.exports = {
   sendAllGames,
   sendCreatedGame,
   sendUpdatedGame,
   sendDeleteGame,
+  sendGameById
 };
